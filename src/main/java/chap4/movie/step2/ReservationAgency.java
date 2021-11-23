c8fa7293fcea622612ecf0565409383ed2a50a31
leejohy-0223 <leejohy@naver.com>
2021-11-23 12:39:09 +0900

    feat: chapter4 add

---
+++ src/main/java/chap4/movie/step2/ReservationAgency.java
@@ -0,0 +1,10 @@
+package chap4.movie.step2;
+
+import chap4.money.Money;
+
+public class ReservationAgency {
+    public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
+        Money fee = screening.calculateFee(audienceCount);
+        return new Reservation(customer, screening, fee, audienceCount);
+    }
+}
