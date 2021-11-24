de9a01cec774e5e30946cf7686db1cf47ae8a7b5
leejohy-0223 <leejohy@naver.com>
2021-11-24 17:46:09 +0900

    feat: chapter6 - 메시지와 인터페이스 실습

---
+++ src/main/java/chap6/theater/step2/Invitation.java
@@ -0,0 +1,7 @@
+package chap6.theater.step2;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ src/main/java/chap6/theater/step2/Ticket.java
@@ -0,0 +1,9 @@
+package chap6.theater.step2;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
